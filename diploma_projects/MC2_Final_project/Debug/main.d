10e62af19a71bb5137fa4c21a8c34034289473d9
merna17798 <mernasaleh1998@gmail.com>
2020-10-16 20:41:12 +0200

    all without driver done

---
+++ diploma_projects/MC2_Final_project/Debug/main.d
@@ -0,0 +1,12 @@
+main.d main.o: ../main.c ../external_eeprom.h ../std_types.h ../uart.h \
+  ../micro_config.h ../common_macros.h
+
+../external_eeprom.h:
+
+../std_types.h:
+
+../uart.h:
+
+../micro_config.h:
+
+../common_macros.h:
